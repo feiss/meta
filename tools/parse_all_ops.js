ca6aac00aa930bcb385a7108801cbbbbd03a8a5e
feiss <github@feiss.be>
2017-03-30 02:27:38 +0200

    added pretty ops.json and ops.min.json

--- tools/parse_all_ops.js
+++ tools/parse_all_ops.js
@@ -6,15 +6,17 @@ var all = {
 	version: '0.1',
 	ops: []
 };
-console.log('=== Looking for operations in '+opsfolder+' ====')
+console.log('=== Looking for operations in '+opsfolder+' ====');
 fs.readdir(opsfolder, (err, files) => {
   files.forEach(file => {
+    if (file[0] === '.') return;
   	var obj = JSON.parse(fs.readFileSync(opsfolder+file, 'utf8'));
   	if (obj){
 	  	all.ops.push(obj);
       console.log('· Added ' + file)
   	}
   });
-  fs.writeFileSync(outputfolder + 'ops.json', JSON.stringify(all));
+  fs.writeFileSync(outputfolder + 'ops.json', JSON.stringify(all, null, 2));
+  fs.writeFileSync(outputfolder + 'ops.min.json', JSON.stringify(all));
   console.log('=== Saved ops.json ====');
 })

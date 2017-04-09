f5113e08f68a45b5b0c6fe853c86e2bd89627a19
feiss <github@feiss.be>
2017-04-10 01:08:14 +0200

    matrix selection fixes

--- bindings/js/matrixselection.js
+++ bindings/js/matrixselection.js
@@ -8,16 +8,17 @@ MatrixSelection.prototype = {
 	clear: function () {
 		this.sel = '';
 		this.rows = [];
-
-		this.iterator = {mat: undefined, rows: 0, cols: 0, it: 0, rowit: 0};
+		this.resetIterator()
 	},
 	setSel: function (s) {
 		this.sel = s || '';
 		this.rows = [];
+		this.resetIterator(this.iterator.mat)
 	},
 	add: function (s) {
 		if (!this.sel) this.sel = s || '';
 		else this.sel += ',' + s;
+		this.resetIterator(this.iterator.mat)
 	},
 	clean: function () {
 		this.sel = this.sel.toLowerCase();
@@ -38,15 +39,24 @@ MatrixSelection.prototype = {
 		}
 		return d;
 	},
-	getCoordsFromPart: function (p) {
+	getCoordsFromPart: function (p, clamp) {
 		var a = p.match(/^[a-z]+/)[0];
 		var b = p.substr(a.length);
-		var col = Math.max( 0, Math.min(this.getAlphaIndex(a), this.iterator.cols));
-		var row = Math.max( 0, Math.min(parseInt(b) - 1, this.iterator.rows));
+		if (clamp) {
+			var col = Math.max( 0, Math.min(this.getAlphaIndex(a), this.iterator.cols));
+			var row = Math.max( 0, Math.min(parseInt(b) - 1, this.iterator.rows));
+		}
+		else {
+			var col = this.getAlphaIndex(a);
+			if (col < 0 || col > this.iterator.cols - 1) return false;
+			var row = parseInt(b) - 1;
+			if (row < 0 || row > this.iterator.rows - 1) return false;
+		}
 		return {rowcol: row, start: col, length: 1, iscolumn: false};
 	},
 	calcRows: function () {
 		// convertir this.sel en arrays [ {rowcol, start, length, iscolumn}, ... ]
+		this.clean();
 		var parts = this.sel.split(',');
 		for (var i = 0; i < parts.length; i++) {
 			var p = parts[i];
@@ -62,7 +72,10 @@ MatrixSelection.prototype = {
 			}
 
 			if (p.indexOf(':') === -1) { // parttype 3 - a1
-					this.rows.push(this.getCoordsFromPart(p));
+				var row = this.getCoordsFromPart(p, false);
+				if (row !== false) {
+					this.rows.push(row);
+				}
 			}
 			else if (parttype == 1 || parttype == 2) { 
 				var [a, b] = p.split(':');
@@ -100,32 +113,38 @@ MatrixSelection.prototype = {
 			}
 		}
 	},
-	normalize: function () {
-		this._calcRows();
-		this._checkColumn();
-	},
-	iterate: function (mat) {
-		if (mat !== this.iterator.mat) {
+	resetIterator: function(mat, recalculateRows) {
+		if (mat !== undefined) {
 			this.iterator.mat = mat;
 			this.iterator.rows = this.iterator.mat.length;
 			this.iterator.cols = this.iterator.mat.length > 0 ? this.iterator.mat[0].length : 0;
 			this.iterator.rowit = 0;
 			this.iterator.it = 0;
-
-			this.calcRows();
+			if (recalculateRows === undefined || recalculateRows === true) this.calcRows();
+		}
+		else {
+			this.iterator = {mat: undefined, rows: 0, cols: 0, it: 0, rowit: 0};
+		}
+	},
+	iterate: function (mat) {
+		if (mat === undefined || mat !== this.iterator.mat) {
+			this.resetIterator(mat);
+			if (mat) return this.iterate(mat);
 		}
 		else {
 			var row = this.rows[this.iterator.rowit];
+			console.log(row)
 			if (this.iterator.it >= row.length){
 				this.iterator.it = 0;
 				this.iterator.rowit ++;
 				if (this.iterator.rowit > this.rows.length) {
+					this.resetIterator(mat, false);
 					return null;
 				}
 			}
 			var colit = row.start + this.iterator.it;
 			this.iterator.it++;
-			if (row.iscolumn) {
+			if (!row.iscolumn) {
 				return { 
 					row: row.rowcol,
 					col: colit,
@@ -136,7 +155,7 @@ MatrixSelection.prototype = {
 				return { 
 					row: colit,
 					col: row.rowcol,
-					value: this.iterator.mat[colit][row.rowcol]
+					value: this.iterator.mat[row.rowcol][colit]
 				}
 			}
 		}

9247c59101806839c01b8c4b95954151e2e8b396
feiss <github@feiss.be>
2017-04-11 01:52:14 +0200

    started textselection.js

--- bindings/js/textselection.js
+++ bindings/js/textselection.js
@@ -1,43 +1,36 @@
-var MatrixSelection = function(s){
-	this.rows = [];
-	this.iterator = {mat: undefined, rows: 0, cols: 0, it: 0, rowit: 0};
+var TextSelection = function(s){
+	this.lines_at = []; // indexes of line beginnings
+	this.lines = [];
+	this.iterator = {text: undefined, length: 0, it: 0, lineit: 0};
 	this.sel = s || '';
 };
 
-MatrixSelection.prototype = {
+TextSelection.prototype = {
 	clear: function () {
 		this.sel = '';
-		this.rows = [];
+		this.lines = [];
 		this.resetIterator()
 	},
 	setSel: function (s) {
 		this.sel = s || '';
-		this.rows = [];
-		this.resetIterator(this.iterator.mat)
+		this.lines = [];
+		this.resetIterator(this.iterator.text)
 	},
 	add: function (s) {
 		if (!this.sel) this.sel = s || '';
 		else this.sel += ',' + s;
-		this.resetIterator(this.iterator.mat)
+		this.resetIterator(this.iterator.text)
 	},
 	clean: function () {
 		this.sel = this.sel.toLowerCase();
-		this.sel = this.sel.replace(/[^\d,:a-z]+/g, "");
+		this.sel = this.sel.replace(/[^\d,:-]+/g, "");
 	},
 	checkPart: function (coords) {
-		if (/^[a-z]+:[a-z]+$/.test(coords)) return 1; // a:b
-		if (/^\d+:\d+$/.test(coords)) return 2; // 3:5
-		if (/^[a-z]+\d+$/.test(coords)) return 3; // a4
-		if (/^[a-z]+\d+:[a-z]+\d+$/.test(coords)) return 4; // a4:b7
+		if (/^\d+$/.test(coords)) return 1; // whole line (1, 3, 15..)
+		if (/^\d+:\d+-\d+$/.test(coords)) return 2; // 1:1-5
+		if (/^\d+:\d+-$/.test(coords)) return 3; // 1:1-
+		if (/^\d+:-\d+$/.test(coords)) return 4; // 1:-5
 		return false;
-	},
-	getAlphaIndex: function (a) {
-		var d;
-		for (var i = a.length - 1, j = 0; i >= 0 ; i--, j++) {
-			if (j === 0) d = a.charCodeAt(i) - 97;
-			else d += (a.charCodeAt(i) - 97 + 1) * Math.pow(26, j);
-		}
-		return d;
 	},
 	getCoordsFromPart: function (p, clamp) {
 		var a = p.match(/^[a-z]+/)[0];
@@ -52,30 +45,41 @@ MatrixSelection.prototype = {
 			var row = parseInt(b) - 1;
 			if (row < 0 || row > this.iterator.rows - 1) return false;
 		}
-		return {rowcol: row, start: col, length: 1, iscolumn: false};
+		return {start: col, end: 1, selected: true};
 	},
+	findLines: function() {
+		this.lines_at = [];
+		if (!this.iterator.text) return;
+		this.lines_at.push(0);
+		var p, lastp;
+		while ( (p = this.iterator.text.indexOf('\n', lastp)) !== -1){
+			if (p < this.iterator.length) {
+				this.lines_at.push(p+1);
+				lastp = p + 1;
+			}
+			else break;
+		}
+	}
 	calcRows: function (clamp) {
 		// convertir this.sel en arrays [ {rowcol, start, length, iscolumn}, ... ]
 		this.clean();
+		this.findLines();
 		var parts = this.sel.split(',');
 		for (var i = 0; i < parts.length; i++) {
 			var p = parts[i];
 			// parttype=
-			// 1  a:b
-			// 2  1:2
-			// 3  a1
-			// 4  a1:b2
+			// 1  1
+			// 2  1:1-5
+			// 3  1:1-
+			// 4  1:-5
 			var parttype = this.checkPart(p);
 			if (!parttype) {
 				console.warn(p + ' is not a valid coordinate(s)');
 				continue;
 			}
 
-			if (p.indexOf(':') === -1) { // parttype 3 - a1
-				var row = this.getCoordsFromPart(p, clamp);
-				if (row !== false) {
-					this.rows.push(row);
-				}
+			if (p.indexOf(':') === -1) { // parttype 1
+				this.lines.push();
 			}
 			else if (parttype == 1 || parttype == 2) { 
 				var [a, b] = p.split(':');
@@ -94,10 +98,10 @@ MatrixSelection.prototype = {
 				if (parttype == 2 && b > this.iterator.rows) b = this.iterator.rows;
 				for (var j = a; j <= b; j++) {
 					if (parttype == 1) {
-						this.rows.push({rowcol: j, start: 0, length: this.iterator.rows, iscolumn: true});
+						this.lines.push({rowcol: j, start: 0, length: this.iterator.rows, iscolumn: true});
 					}
 					else {
-						this.rows.push({rowcol: j, start: 0, length: this.iterator.cols, iscolumn: false});
+						this.lines.push({rowcol: j, start: 0, length: this.iterator.cols, iscolumn: false});
 					}
 				}
 			}
@@ -109,42 +113,41 @@ MatrixSelection.prototype = {
 				var start = Math.min(aa.start, bb.start);
 				var length = Math.max(aa.start, bb.start) - start + 1;
 				for (var r = aa.rowcol; r <= bb.rowcol; r++) {
-					this.rows.push({rowcol: r, start: start, length: length ,iscolumn: false})
+					this.lines.push({rowcol: r, start: start, length: length ,iscolumn: false})
 				}
 			}
 		}
 	},
-	resetIterator: function(mat, recalculateRows) {
-		if (mat !== undefined) {
-			this.iterator.mat = mat;
-			this.iterator.rows = this.iterator.mat.length;
-			this.iterator.cols = this.iterator.mat.length > 0 ? this.iterator.mat[0].length : 0;
-			this.iterator.rowit = 0;
+	resetIterator: function(text, recalculateRows) {
+		if (text !== undefined) {
+			this.iterator.text = text;
+			this.iterator.length = text.length;
 			this.iterator.it = 0;
+			this.iterator.lineit = 0;
 			if (recalculateRows === undefined || recalculateRows === true) {
 				this.calcRows();
 			}
 		}
 		else {
-			this.iterator = {mat: undefined, rows: 0, cols: 0, it: 0, rowit: 0};
+			this.iterator = {mat: undefined, length: 0, it: 0, lineit: 0};
 		}
 	},
 	iterate: function (mat) {
-		if (mat === undefined || mat !== this.iterator.mat) {
+		if (mat === undefined || mat !== this.iterator.text) {
 			this.resetIterator(mat);
 			if (mat) return this.iterate(mat);
 		}
 		else {
-			var row = this.rows[this.iterator.rowit];
+			var row = this.lines[this.iterator.lineit];
 			if (this.iterator.it >= row.length){
 				this.iterator.it = 0;
-				this.iterator.rowit ++;
-				if (this.iterator.rowit >= this.rows.length) {
+				this.iterator.lineit ++;
+				if (this.iterator.lineit >= this.lines.length) {
 					this.resetIterator(mat, false);
 					return null;
 				}
 				else {
-					row = this.rows[this.iterator.rowit];
+					row = this.lines[this.iterator.lineit];
 				}
 			}
 			var colit = row.start + this.iterator.it;
@@ -164,4 +167,5 @@ MatrixSelection.prototype = {
 		}
 	}	
 };
+
 
